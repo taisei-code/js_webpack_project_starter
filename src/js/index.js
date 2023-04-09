57f0d042f5e8d03d18d3279fa19d8e1cff6b3d4f
Taisei <taisei96.web@gmail.com>
2023-04-09 18:46:15 +0900

    ナビゲーション作成

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,5 @@
-const nav = document.querySelector('#nav');
+const nav      = document.querySelector('#nav');
+const cover    = document.querySelector('#cover');
 const appNames = ['todo', 'typing', 'slide-puzzle', 'memory-card', 'life'];
 
 appNames.forEach(appName => {
@@ -6,5 +7,20 @@ appNames.forEach(appName => {
   menu.classList.add('nav-menu');
   menu.textContent = appName.toUpperCase();
   
+  menu.addEventListener('click', () => {
+    cover.classList.remove('active');
+    const appEls = document.querySelectorAll('.app');
+    appEls.forEach(appEl => {
+      appEl.classList.remove('active');
+    })
+    const appEl  = document.getElementById(appName);
+    appEl.classList.add('active');
+    const navMenus = document.querySelectorAll('.nav-menu');
+    navMenus.forEach(navMenu => {
+      navMenu.classList.remove('active');
+    })
+    menu.classList.add('active');
+  })
+
   nav.appendChild(menu);
 })
