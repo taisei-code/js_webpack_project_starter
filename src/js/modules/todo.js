f43e1b1377189a03deec5389db5af76a4be085a4
Taisei <taisei96.web@gmail.com>
2023-04-10 08:51:47 +0900

    Todoアプリ/データ保存

--- src/js/modules/todo.js
+++ src/js/modules/todo.js
@@ -1,1 +1,21 @@
-console.log('from todo.js');
+const addForm  = document.querySelector('.td-add-form');
+const addInput = document.querySelector('.td-add-input');
+
+let todoData = [];
+
+addForm.addEventListener('submit', e => {
+  e.preventDefault();
+  let todoObj = {
+    content: addInput.value.trim(),
+    isDone: false
+  };
+  if(todoObj.content) {
+    todoData.push(todoObj);
+  }
+  addInput.value = '';
+  updateLS();
+})
+
+function updateLS() {
+  localStorage.setItem('myTodo', JSON.stringify(todoData));
+}
