c7143ebf7346a14d9e9b118fb1ccd03d666ecd48
Taisei <taisei96.web@gmail.com>
2023-04-10 09:39:30 +0900

    Todoアプリ/データ取得

--- src/js/modules/todo.js
+++ src/js/modules/todo.js
@@ -1,5 +1,7 @@
 const addForm  = document.querySelector('.td-add-form');
 const addInput = document.querySelector('.td-add-input');
+const todosUl  = document.querySelector('.todos');
+const donesUl  = document.querySelector('.dones');
 
 let todoData = [];
 
@@ -19,3 +21,37 @@ addForm.addEventListener('submit', e => {
 function updateLS() {
   localStorage.setItem('myTodo', JSON.stringify(todoData));
 }
+
+function getTodoData() {
+  return JSON.parse(localStorage.getItem('myTodo'));
+}
+
+function createTodoElement(todo) {
+  const todoItem          = document.createElement('li');
+  const todoContent       = document.createElement('p');
+  todoContent.textContent = todo.content;
+  todoItem.appendChild(todoContent);
+
+  const btnContainer = document.createElement('div');
+  const btn          = document.createElement('img');
+  btn.classList.add('td-btn');
+  const upBtn = btn.cloneNode(false);
+  upBtn.setAttribute('src', './images/todo_button/up.png');
+
+  if(!todo.isDone) {
+    btn.setAttribute('src', './images/todo_button/ok.png');
+    btnContainer.appendChild(btn);
+    btnContainer.appendChild(upBtn);
+    todoItem.appendChild(btnContainer);
+    todosUl.appendChild(todoItem);
+  } else {
+    btn.setAttribute('src', './images/todo_button/cancel.png');
+    btnContainer.appendChild(btn);
+    btnContainer.appendChild(upBtn);
+    todoItem.appendChild(btnContainer);
+    donesUl.appendChild(todoItem);
+  }
+}
+
+createElement({content: 'remans to do', isDone: false})
+createElement({content: 'already', isDone: true})
